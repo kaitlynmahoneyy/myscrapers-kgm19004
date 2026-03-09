8a31c07b1a6978c737eca67d62e496dc89802885
Kaitlyn Mahoney <70338199+kaitlynmahoneyy@users.noreply.github.com>
2026-03-09 16:48:58 -0400

    Update main.py

--- cloud_function/extractor-per-listing/main.py
+++ cloud_function/extractor-per-listing/main.py
@@ -39,8 +39,8 @@ storage_client = storage.Client()
 PRICE_RE      = re.compile(r"\$\s?([0-9,]+)")
 YEAR_RE       = re.compile(r"\b(19|20)\d{2}\b")
 MAKE_MODEL_RE = re.compile(r"\b([A-Z][a-z]+)\s+([A-Z][A-Za-z0-9]+)")
-TRANSMISSION_RE = re.compile(r"\b(automatic|manual|cvt|auto)\b", re.I)  # Added transmission and it is case sensitive
-FUEL_RE         = re.compile(r"\b(gas|gasoline|diesel|electric|hybrid|plug-in hybrid)\b", re.I) # Added fuel and it is case sensitive
+TRANSMISSION_RE = re.compile(r"\b(automatic|manual|cvt|auto)\b", re.I)  # Added transmission and it is case insensitive
+FUEL_RE         = re.compile(r"\b(gas|gasoline|diesel|electric|hybrid|plug-in hybrid)\b", re.I) # Added fuel and it is case insensitive
 
 # -------------------- HELPERS --------------------
 def _list_run_ids(bucket: str, scrapes_prefix: str) -> list[str]:
